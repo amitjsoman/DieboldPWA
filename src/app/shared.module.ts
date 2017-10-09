564d1aa680d701be38250c3b3cc21ca26692e0b7
amitjsoman <amitjsoman@gmail.com>
2017-10-09 10:18:00 +0530

    Delete shared.module.ts

--- src/app/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import {NgModule} from '@angular/core';
-import {IonicPageModule} from 'ionic-angular';
-import {ExpandableComponent} from '../components/expandable/expandable';
-
-@NgModule({
-  imports: [IonicPageModule.forChild(ExpandableComponent)],
-  declarations: [ExpandableComponent],
-  exports: [ExpandableComponent]
-}) export class SharedModule {}
